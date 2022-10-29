e6f3cb4660a41a8bd016614b9fb84c38274b2479
DustyFate <quanghuy7927@gmail.com>
2022-10-29 17:21:28 +0700

    up more file

--- login.js
+++ login.js
@@ -1,6 +1,12 @@
 
 
-test()
+function test()
 {
-    alert("hello");
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "1" && password == "1") {
+        window.location = "RemoteControl/index.html";
+    } else {
+        alert("wrong username or password");
+    }
 }
