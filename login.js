90ca3897f7aedb06308347fe99e509a179363f5b
DustyFate <quanghuy7927@gmail.com>
2022-10-29 17:29:10 +0700

    rena

--- login.js
+++ login.js
@@ -6,7 +6,12 @@ function test()
     var password = document.getElementById("password").value;
     if (username == "1" && password == "1") {
         window.location = "RemoteControl/index.html";
-    } else {
+    } 
+    else if (username == "rena" && password == "rena")
+    {
+        window.location = "ScanPhoto/index.html";
+    }
+    else {
         alert("wrong username or password");
     }
 }
